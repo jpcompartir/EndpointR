0e6f045314067b31ff086d7dc2a4418edd51cf60
jpcompartir <jp1689@my.bristol.ac.uk>
2025-04-10 11:18:50 +0100

    Add the start of the data frame approach

--- R/hf_embeddings.R
+++ R/hf_embeddings.R
@@ -3,7 +3,13 @@ library(stringr)
 library(jsonlite)
 library(purrr)
 
-# basic func for getting the list.
+embed_hf <- function(endpoint_url, api_key = api_key)
+
+batch_embed_hf <- function(endpoint_url, api_key = api_key, batch_size = 10, max_retries = 5) {
+
+}
+
+# basic func for getting the list tidied.
 tidy_nested_embedding_list <- function(nested_list) {
   tib <- sapply(nested_list, unlist) |>  # first conv to matrix, but this is long format
     t() |> # transpose to wide form
@@ -26,7 +32,9 @@ req <- req |>
   req_method("POST") |>
   req_headers("Content-Type" = "application/json") |>
   req_auth_bearer_token(token = api_key) |>
-  req_body_json(list(inputs = sentences[1:30])) |>
+  req_progress() |>
+  req_body_json(list(inputs = sentences[[1]])) |>
+  # req_body_json(list(inputs = sentences[31:60])) |>
   req_retry(
     max_tries = 10,
     retry_on_failure = TRUE)
@@ -45,3 +53,22 @@ sapply(resp_json, unlist) |>  # first conv to matrix, but this is long format
   t() |> # transpose to wide form
   as.data.frame.matrix() |>  # conv to df, has grim column names
   tibble::as_tibble() # get nice column names
+
+
+# df approach - need to figure out the best way to handle batches, and whether to cache or not - I think caching is probably a good idea.
+id <-paste0("uid_", 1:length(sentences))
+request_df <- tibble(
+  id = id,
+  text = sentences
+) |>
+  mutate(request = map(text, ~ build_hf_embed_request(.x, hf_test_api_url, api_key = api_key)
+  ))
+
+response_df <-request_df |>
+  mutate(response = map(request, ~ req_perform(.x)))
+
+resp <- req |>
+  req_perform(path = )
+
+
+response_df |>  pluck('response', 1) |>  resp_body_json() |>  tidy_nested_embedding_list()
