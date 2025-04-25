e274fe8cf2c286fcddffdc70a93c1096579204e9
jpcompartir <jp1689@my.bristol.ac.uk>
2025-04-25 13:23:03 +0100

    Update the HF Inference vignette with batching info and move towards a more logical order. fix stub logic in test-hf_embed

--- tests/testthat/test-hf_embed.R
+++ tests/testthat/test-hf_embed.R
@@ -1,9 +1,11 @@
 test_that("validate_hf_endpoint errors if given invalid URL", {
 
+  mockery::stub(validate_hf_endpoint, "get_api_key", "HF_TEST_API_KEY")
   expect_error(
     validate_hf_endpoint(
       endpoint_url = "xxx_yy.com",
-      key_name = mockery::stub("HF_TEST_API_KEY")),
+      key_name = "HF_TEST_API_KEY"
+      ),
     regexp = "Cannot connect to Hugging")
 
   # testing strategy is a bit difficult as we're working with endpoints that won't be up etc. so we don't *really* want to do check for success, at least not when CI/CD runs. Need to think more on this, they may just be local tests for most things - which is not ideal.
@@ -13,6 +15,6 @@ test_that("validate_hf_endpoint errors if given invalid URL", {
 
 })
 
-test_that("tidy_embedding_respons tidies single requests and batches", {
-
-})
+# test_that("tidy_embedding_response tidies single requests and batches", {
+#
+# })
