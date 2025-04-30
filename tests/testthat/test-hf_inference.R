a929e05e467fce49058e37647e24dd5d0a621f80
jpcompartir <jp1689@my.bristol.ac.uk>
2025-04-30 10:07:11 +0100

    switch request tests to withr::with_envvar() over mockery::stub

--- tests/testthat/test-hf_inference.R
+++ tests/testthat/test-hf_inference.R
@@ -13,76 +13,69 @@ test_that("hf_build_request does not try to handle batches", {
   # what's the best way to handle this, shouold I spin up a tmp .Renviron file and put the a key in, or just use HF_TEST_API_KEY? I don't kike the latter as it won't be obvious to anyone else who develops and it will be extra work in CI/CD
   endpoint_url <- "https://gibberish.com"
 
-  mockery::stub(hf_build_request, "get_api_key", "TEST_API_KEY") # as hf_build_request calls get_api_key, we need to make sure we return a string of the right form
+  withr::with_envvar(
+    c("TEST_API_KEY" = "gibberish"),
+    {
+      expect_error(
+        hf_build_request(
+          input = list("hello", "number 2"),
+          endpoint_url =endpoint_url,
+          key_name = "TEST_API_KEY"))
 
-  expect_error(hf_build_request(input = list("hello", "number 2"),
-                   endpoint_url =endpoint_url,
-                   key_name = "TEST_API_KEY"))
-
-  test_req <- expect_no_error(hf_build_request(input = "hello",
-                                   endpoint_url = endpoint_url,
-                                   key_name = "TEST_API_KEY"))
-
+      test_req <- expect_no_error(hf_build_request(input = "hello",
+                                                   endpoint_url = endpoint_url,
+                                                   key_name = "TEST_API_KEY"))
+    })
   expect_true(test_req$body$data$inputs == "hello")
 
 })
 
 test_that("hf_build_request_batch handles batches",{
   endpoint_url <- "https://gibberish.com"
-  mockery::stub(hf_build_request_batch, "get_api_key", "TEST_API_KEY")
   texts <- list("batch text1", "batch text2")
-
-  test_batch_req <- expect_no_error(hf_build_request_batch(inputs = texts, endpoint_url = endpoint_url, key_name = "TEST_API_KEY"))
-
-  expect_length(test_batch_req$body$data$inputs, 2)
-
-})
-
-
-test_that("hf_build_request_df's arguments are functioning", {
-  texts <- paste0("text", 1:10)
-  ids <- seq(1:length(texts))
-  test_df <- data.frame(id = ids, text = texts)
-  endpoint_url <- "gibberish.com"
-  key_name <- "TEST_API_KEY"
-
-  mockery::stub(hf_build_request, "get_api_key", "TEST_API_KEY")
-
-  hf_build_request_df(
-    df = test_df,
-    text_var = text,
-    id_var = id,
-    endpoint_url = endpoint_url,
-    key_name = key_name
-  )
-
-})
-
-
-test_that("hf_build_request_df's arguments are functioning", {
-  texts <- paste0("text", 1:10)
-  ids <- seq(1:length(texts))
-  test_df <- data.frame(id = ids, text = texts)
-  endpoint_url <- "http://gibberish.com"
   key_name <- "TEST_API_KEY"
 
   withr::with_envvar(
     c("TEST_API_KEY" = "gibberish"),
     {
-      result <- hf_build_request_df(
-        df = test_df,
-        text_var = text,
-        id_var = id,
-        endpoint_url = endpoint_url,
-        key_name = key_name,
-        max_retries = 10,
-        timeout = 18
-      )
-      expect_true(".request" %in% names(result))
-    }
-  )
+      test_batch_req <-
+        expect_no_error(
+          hf_build_request_batch(
+            inputs = texts,
+            endpoint_url = endpoint_url,
+            key_name = key_name))
+    })
 
+  expect_length(test_batch_req$body$data$inputs, 2)
+})
+
+
+test_that("hf_build_request_df's arguments are functioning", {
+  texts <- paste0("text", 1:10)
+  ids <- seq(1:length(texts))
+  test_df <- data.frame(id = ids, text = texts)
+  endpoint_url <- "https://gibberish.com"
+  key_name <- "TEST_API_KEY"
+
+  withr::with_envvar( # preferable to mockery::stub
+    c("TEST_API_KEY" = "gibberish"),
+    {
+      result <-
+        expect_no_error(
+          hf_build_request_df(
+            df = test_df,
+            text_var = text,
+            id_var = id,
+            endpoint_url = endpoint_url,
+            key_name = key_name,
+            max_retries = 10,
+            timeout = 18))
+      })
+
+  expect_true(".request" %in% names(result))
   single_request <- result$.request[[1]]
   expect_s3_class(single_request, "httr2_request")
   expect_equal(single_request$options$timeout_ms, 18000)
+
+  expect_equal(single_request$policies$retry_max_tries, 10)
 })
