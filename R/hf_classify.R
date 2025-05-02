fbf3e9984e9a02f22b84b9aa717ff8371e990dd3
jpcompartir <jp1689@my.bristol.ac.uk>
2025-05-02 11:18:17 +0100

    commit tidy classification response function, first draft

--- R/hf_classify.R
+++ R/hf_classify.R
@@ -3,6 +3,7 @@
 # functions from hf_embed serve as :sparkles: inspo :sparkles:
 
 tidy_classification_response <- function(response){
+
   if (inherits(response, "httr2_response")) {
     resp_json <- httr2::resp_body_json(response)
   } else {
@@ -12,6 +13,16 @@ tidy_classification_response <- function(response){
   # sort later, we're basically going to unlist and pivot?
   # might be better for users to build these themselves and we handle
   # peaks and pits, sentiment, or something
+
+  tidy_response <-
+    purrr::flatten(resp_json) |>
+      purrr::map(~ data.frame(label = .x$label,
+                            score = .x$score)) |>  # will need to wrap this in a tryCatch.
+      purrr::list_rbind() |>
+      tidyr::pivot_wider(names_from = label, values_from = score)
+
+
+  return(tidy_response)
 }
 
 # hf_classify_docs ----
