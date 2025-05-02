ba0af43e9f13adace1d91e17a2b07cb63a1eeba2
jpcompartir <jp1689@my.bristol.ac.uk>
2025-05-02 11:17:43 +0100

    add hf_classify_text func v1

--- R/hf_classify.R
+++ R/hf_classify.R
@@ -14,4 +14,55 @@ tidy_classification_response <- function(response){
   # peaks and pits, sentiment, or something
 }
 
+# hf_classify_docs ----
+hf_classify_text <- function(text,
+                             endpoint_url,
+                             key_name,
+                             ...,
+                             parameters = list(return_all_scores = TRUE),
+                             tidy = TRUE,
+                             max_retries = 5,
+                             timeout = 20,
+                             validate = FALSE
+                             ) {
+
+  stopifnot(
+    "Text must be a character vector" = is.character(text)
+  )
+  api_key <- get_api_key(key_name)
+
+  req <-  req <- hf_build_request(input = text,
+                                  parameters = parameters,
+                                  endpoint_url = endpoint_url,
+                                  key_name = key_name,
+                                  max_retries = max_retries,
+                                  timeout = timeout,  # longer for classification than embedding default
+                                  validate = validate)
+
+  tryCatch({
+    response <- hf_perform_request(req, ...)
+  }, error = function(e) {
+    cli::cli_abort(c(
+      "Failed to generate classification",
+      "i" = "Text: {cli::cli_vec(text, list('vec-trunc' = 30, 'vec-sep' = ''))}",
+      "x" = "Error: {conditionMessage(e)}"
+    ))
+  })
+
+
+  if (!tidy) { return(response)}
+
+  tryCatch({
+    tidy_classification_response(response)
+  }, error = function(e) {
+    cli::cli_alert_info("Failed to tidy output")
+    cli::cli_bullets(c(
+      "i" = "Text: {cli::cli_vec(text, list('vec-trunc' = 30, 'vec-sep' = ''))}",
+      "x" = "Error: {conditionMessage(e)}",
+      " " = "Returning un-tidied response, tidy manually."
+    ))
+    return(response)
+  })
+
+}
 
