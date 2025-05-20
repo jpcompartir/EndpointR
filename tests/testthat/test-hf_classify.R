c379ed0fd71c55d2b738adc6fa36ac7ceb8c7560
jpcompartir <jp1689@my.bristol.ac.uk>
2025-05-20 15:06:12 +0100

    add updated tests for `hf_classify_batch()`, think we'll need to move on though now.

--- tests/testthat/test-hf_classify.R
+++ tests/testthat/test-hf_classify.R
@@ -71,7 +71,7 @@ test_that("tidy_batch_classification_response handes a batch response with multi
 test_that("hf_classify_text takes a text and returns a tidied classification response", {
 
 
-  # there's something I don't understand with webfakes, where these tests will pass when run in the `test_that()` function call, but the code will not *always* run when outside of that context. There will sometimes by a HTTP 500 Internal Server Error,which I think means the app is dying. I think to test individually we need to use $listen() rather than $local_app_process or $new_app_process but I don't fully understand why.
+  # there's something I don't understand with webfakes, where these tests will pass when run in the `test_that()` function call, but the code will not *always* run when outside of that context. There will sometimes be a HTTP 500 Internal Server Error,which I think means the app is dying before the requests are sent. I think to test individually we need to use $listen() rather than $local_app_process or $new_app_process but I don't fully understand why.
 
   json_string <- '[[{"label":"positive","score":0.05167632},{"label":"negative","score":0.8648104},{"label":"neutral","score":0.0835133}]]'
 
@@ -131,4 +131,79 @@ test_that("hf_classify_text takes a text and returns a tidied classification res
   expect_equal(tidy_result$positive, 0.05167632, tolerance = 1e-6)
   expect_equal(tidy_result$negative, 0.8648104, tolerance = 1e-6)
   expect_equal(tidy_result$neutral, 0.0835133, tolerance = 1e-6)
+
+  server$stop()
 })
+
+test_that("hf_classify_batch processes a batch of texts and returns a tidied classification response",{
+
+  # mocking these batches not straightforward, as the input and the app's output aren't linked (has to be hardcoded). For that reason it's difficult to actually test the batching logic.
+
+  # withr::deferred_clear() # uncomment if interactively running/developing tests
+
+  .app <- webfakes::new_app()
+  .app$post("/test", function(req, res){
+    response_data <- list(
+      list(
+        list(label = "positive", score = 0.5866),
+        list(label = "negative", score = 0.1205),
+        list(label = "neutral", score = 0.293)
+      ),
+      list(
+        list(label = "positive", score = 0.5067),
+        list(label = "negative", score = 0.1104),
+        list(label = "neutral", score = 0.3829)
+      ),
+      list(
+        list(label = "positive", score = 0.65),
+        list(label = "negative", score = 0.00),
+        list(label = "neutral", score = 0.35)
+      ),
+      list(
+        list(label = "positive", score = 0.65),
+        list(label = "negative", score = 0.00),
+        list(label = "neutral", score = 0.35)
+      )
+    )
+
+    res$set_header("Content-Type", "application/json")
+
+    json_string <- jsonlite::toJSON(response_data, auto_unbox = TRUE)
+    res$send(json_string)
+  })
+
+  .web <- webfakes::local_app_process(.app)
+  # withr::deferred_run() # uncomment if interactively running/developing tests
+  .test_url <- .web$url("/test")
+
+  test_response <- httr2::request(.test_url) |>
+    httr2::req_method("POST") |>
+    httr2::req_perform()
+
+
+  # test_response |>
+  #   httr2::resp_body_string() # uncomment to view response
+
+  parsed_response <- test_response |>
+    httr2::resp_body_json()
+
+  # print(parsed_response)
+
+  res <- hf_classify_batch(
+    texts = c("1", "2", "3", "4"),
+    endpoint_url = .test_url,
+    key_name = "HF_TEST_API_KEY",
+    max_retries = 1,
+    batch_size = 4,
+    concurrent_requests = 1,
+    include_texts = FALSE
+  )
+
+
+  expect_equal(nrow(res), 4)
+  expect_setequal(names(res), c("positive", "negative", "neutral", ".error", ".error_message"))
+
+  .web$stop()
+
+})
+
