8b300eb65d8c8353ae41f4fb7e124596502ee558
jpcompartir <jp1689@my.bristol.ac.uk>
2025-06-17 15:48:43 +0100

    add the base request and some tidying of the outputs for oai_embeddings

--- R/openai_embed.R
+++ R/openai_embed.R
@@ -1,21 +1,75 @@
+# Cannot input empty string
+# Inputs (combined) cannot exceed 8092 tokens (max embed. length)
+# Can input dimensions
+
 oai_build_embedding_request <- function(input,
                                         model = "text-embedding-3-small",
-                                        timeout = 20,
+                                        dimensions = NULL,
                                         max_retries = 5,
+                                        timeout = 20,
                                         endpoint_url = "https://api.openai.com/v1/embeddings",
                                         key_name = "OPENAI_API_KEY") {
 
+  api_key <- get_api_key(key_name)
+
+  request <- base_request(endpoint_url = endpoint_url,
+                          api_key = api_key)
+
+  request <- request |>
+    httr2::req_body_json(list(
+      input = input,
+      model = model,
+      dimensions = dimensions,
+      encoding_format = "float"
+      )) |>
+    httr2::req_timeout(timeout) |>
+    httr2::req_retry(max_tries = max_retries,
+                     backoff = ~ 2 ^ .x,
+                     retry_on_failure = TRUE)
+
+
+
+
+
+
+
 }
 
 
-oai_embed_text <- function(text, model = "text-embedding-3-small",endpoint_url = "https://api.openai.com/v1/embeddings", key_name = "OPENAI_API_KEY") {
+oai_embed_text <- function(text,
+                           model = "text-embedding-3-small",
+                           dimensions = NULL,
+                           max_retries = 5,
+                           timeout = 20,
+                           endpoint_url = "https://api.openai.com/v1/embeddings",
+                           key_name = "OPENAI_API_KEY") {
 
 }
 
-oai_embed_list <- function(texts, model = "text-embedding-3-small", concurrent_requests = 1, max_retries = 5, timeout = 20, endpoint_url = "https://api.openai.com/v1/embeddings", key_name = "OPENAI_API_KEY") {
+oai_embed_batch <- function(texts,
+                           model = "text-embedding-3-small",
+                           dimensions = NULL,
+                           batch_size = 1,
+                           concurrent_requests = 1,
+                           max_retries = 5,
+                           timeout = 20,
+                           endpoint_url = "https://api.openai.com/v1/embeddings",
+                           key_name = "OPENAI_API_KEY") {
+
+  # can take a list natively
 
 }
 
-oai_embed_df <- function(df, text_var, id_var, max_retries = 5, timeout = 20, model = "text-embedding-3-small",endpoint_url = "https://api.openai.com/v1/embeddings", key_name = "OPENAI_API_KEY" ) {
+oai_embed_df <- function(df,
+                         text_var,
+                         id_var,
+                         model = "text-embedding-3-small",
+                         dimensions = NULL,
+                         batch_size = 1,
+                         concurrent_requests = 1,
+                         max_retries = 5,
+                         timeout = 20,
+                         endpoint_url = "https://api.openai.com/v1/embeddings",
+                         key_name = "OPENAI_API_KEY" ) {
 
 }
