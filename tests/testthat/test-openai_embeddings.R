5d67783184448e7a500cb208811ac667f81bb3d3
jpcompartir <jp1689@my.bristol.ac.uk>
2025-06-19 11:55:09 +0100

    add start of tests for tidy_oai_embedding()

--- tests/testthat/test-openai_embeddings.R
+++ tests/testthat/test-openai_embeddings.R
@@ -1,3 +1,29 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("tidy_oai_embedding handles single and batches",{
+
+  mock_single <- list(
+    list(
+      object = "embedding",
+      index = 0,
+      embedding = list(0.1, 0.2, 0.3, 0.4, 0.5)
+    )
+  )
+
+  mock_batch <- list(
+    list(
+      object = "embedding",
+      index = 0,
+      embedding = list(0.1, 0.2, 0.3)
+    ),
+    list(
+      object = "embedding",
+      index = 1,
+      embedding = list(0.2, 0.3, 0.4)
+    )
+  )
+
+  tidied_single <- tidy_oai_embedding(mock_single)
+  tidied_batch <- tidy_oai_embedding(mock_batch)
+
+
 })
+
