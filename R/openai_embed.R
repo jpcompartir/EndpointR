e6f1668ef884bba91c9b5a88bc2c310fbab48de9
jpcompartir <jp1689@my.bristol.ac.uk>
2025-06-19 11:54:21 +0100

    progress on oai_embed_text

--- R/openai_embed.R
+++ R/openai_embed.R
@@ -48,6 +48,7 @@ tidy_oai_embedding <- function(response_data) {
     index_list <- .x$index
     embeddings_list <- .x$embedding
 
+
     col_names <- paste0("V", seq_along(embeddings_list))
     embeddings_list <- setNames(embeddings_list, col_names)
 
@@ -61,6 +62,7 @@ tidy_oai_embedding <- function(response_data) {
 
 }
 
+# Checks that text is a length 1 vector, i.e. not batch
 oai_embed_text <- function(text,
                            model = "text-embedding-3-small",
                            dimensions = NULL,
@@ -69,8 +71,35 @@ oai_embed_text <- function(text,
                            endpoint_url = "https://api.openai.com/v1/embeddings",
                            key_name = "OPENAI_API_KEY") {
 
+  stopifnot("Text must be a character vector" = is.character(text))
+
+  request <- oai_build_embedding_request(
+    input = text,
+    model = model,
+    dimensions = dimensions,
+    max_retries = max_retries,
+    timeout = timeout,
+    endpoint_url = endpoint_url,
+    key_name = key_name
+  )
+
+  response <- hf_perform_request(request)
+
+  status <- response$status_code
+  date <- parse_oai_date(response$headers$date)
+  requests_remaining <- response$headers$`x-ratelimit-reset-requests`
+  requests_reset_ms <- response$headers$`x-ratelimit-reset-requests`
+
+  response_body_json <- httr2::resp_body_json(response) # object, data, model, usage
+
+  response_body_json_data <- response_json$data  # top-level = list -> object, index, embedding.
+  # fire this list into tidy_oai_embedding,
+
+
+
 }
 
+# Checks that texts is a length > 1 vector, i.e. not single text
 oai_embed_batch <- function(texts,
                            model = "text-embedding-3-small",
                            dimensions = NULL,
@@ -81,7 +110,8 @@ oai_embed_batch <- function(texts,
                            endpoint_url = "https://api.openai.com/v1/embeddings",
                            key_name = "OPENAI_API_KEY") {
 
-  # can take a list natively
+  # can take a list natively with the OpenAI embeddings.
+  # data$embedding, data$index in the response if batch
 
 }
 
