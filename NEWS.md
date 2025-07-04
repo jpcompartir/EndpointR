91bcbef49741cdd8b3a51e2cda09b5f3a4a0bb18
jpcompartir <jp1689@my.bristol.ac.uk>
2025-07-04 12:59:38 +0100

    add .handle_output_filename and tests

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,8 @@
+# EndpointR 0.1.1
+
+-   `oai_complete_chunks()` function to better support for chunking/batching in `oai_complete_df()`
+-   `oai_complete_df()` now writes to a file to mitigate the chance of completely lost data
+
 # EndpointR 0.1.0
 
 Initial BETA release, ships with:
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -17,3 +17,24 @@ test_that("batch_vector takes a vector input and batches according to batch_size
 
   batched_3 <- expect_no_error(batch_vector(batch_texts, 3))
 })
+
+
+test_that(".handle_output_filename deals with auto, NULL, strings, and has input validation where needed",{
+
+  expect_error(.handle_output_filename(x ="bosh", base_file_name = "oai_batch"),
+               regexp = "must have a .csv")
+
+  auto_file_name <- expect_no_error(.handle_output_filename(x ="auto", base_file_name = "oai_batch"))
+
+  expect_true(grepl("oai_batch_.*csv$", auto_file_name))
+
+  temp_file_name <- expect_no_error(.handle_output_filename(x = NULL))
+
+  expect_true(grepl("batch_processing", temp_file_name))
+
+
+  valid_file_name <-
+    expect_no_error(.handle_output_filename("oai_batch.csv"))
+
+  expect_true(identical(valid_file_name, "oai_batch.csv"))
+})
