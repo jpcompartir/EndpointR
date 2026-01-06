0a1eebbe7c583bc6291a69f51e4afff0adddc14d
jpcompartir <jp1689@my.bristol.ac.uk>
2026-01-06 17:17:54 +0000

    add oai_batch_file_upload but may revise this to oai_file_upload

--- R/oai_batch_api.R
+++ R/oai_batch_api.R
@@ -50,22 +50,25 @@ oai_batch_prepare_embeddings <- function(df, text_var, id_var, model = "text-emb
   return(reqs)
 }
 
-oai_batch_file_upload <- function(jsonl_rows, key_name = "OPENAI_API_KEY") {
+oai_batch_file_upload <- function(jsonl_rows, key_name = "OPENAI_API_KEY", purpose = "batch") {
 
   api_key <- get_api_key(key_name)
 
-  tmp <- tempfile(fileext = ".jsonl")
-  on.exit(unlink(tmp)) # if session crashes we drop the file from mem safely
-  writeLines(jsonl_rows, tmp) # send the content to the temp file for uploading to OAI
+  .tmp <- tempfile(fileext = ".jsonl")
+  on.exit(unlink(.tmp)) # if session crashes we drop the file from mem safely
+  writeLines(jsonl_rows, .tmp) # send the content to the temp file for uploading to OAI
   # question here is whether to also save this somewhere by force...
   # once OAI have the file it's backed up for 30 days.
 
   httr2::request(base_url = "https://api.openai.com/v1/files") |>
     httr2::req_auth_bearer_token(api_key) |>
-    httr2::req_body_multipart(file = curl::form_file(tmp),
-                              purpose = "batch") |>
+    httr2::req_body_multipart(file = curl::form_file(.tmp),
+                              purpose = purpose) |>
+    httr2::req_error(is_error = ~ FALSE) |>
     httr2::req_perform() |>
     httr2::resp_body_json()
+
+}
 
 oai_file_list <- function(purpose = "batch", key_name = "OPENAI_API_KEY") {
 
